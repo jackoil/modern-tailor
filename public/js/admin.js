9db335893a6102f60f9498bb5958f5f8257bc8ca
mahdi <mahdi.shahpuri.arani@gmail.com>
2025-05-03 12:15:10 +0330

    stash

--- public/js/admin.js
+++ public/js/admin.js
@@ -58995,7 +58995,7 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkmodern_taillor"] = self["webpackChunkmodern_taillor"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
